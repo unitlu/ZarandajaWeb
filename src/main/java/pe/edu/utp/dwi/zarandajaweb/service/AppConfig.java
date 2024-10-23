80d96cc6b20acf181bc0d75c60403f91deca2d72
Paul <josepaulcamposterrones@gmail.com>
2024-10-23 00:40:47 -0500

    conexion con bd log file TextUTP Auth app.properties nuevas carpetas pa mvc

---
+++ src/main/java/pe/edu/utp/dwi/zarandajaweb/service/AppConfig.java
@@ -0,0 +1,14 @@
+package pe.edu.utp.dwi.zarandajaweb.service;
+
+import java.util.ResourceBundle;
+
+public class AppConfig {
+    static ResourceBundle rb = ResourceBundle.getBundle("app");
+    public static String getDatasource(){
+        return rb.getString("datasource");
+    }
+
+    public static String getErrorLogFile(){
+        return rb.getString("errorLog");
+    }
+}
